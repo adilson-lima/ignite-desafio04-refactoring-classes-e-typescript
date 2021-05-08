acf37603137cca4e7921b31f32524beecaf26038
Adilson Pereira Lima <adilson.pereira.lima@gmail.com>
2021-05-08 08:57:53 -0300

    Desafio concluído

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,37 @@
-import { Component } from 'react';
 import { FiPlusSquare } from 'react-icons/fi';
 
 import { Container } from './styles';
 import Logo from '../../assets/logo.svg';
 
+interface HeaderProps {
+  openModal: () => void;
+}
+
+const Header: React.FC<HeaderProps> = ({openModal}: HeaderProps) => {
+
+  return (
+    <Container>
+      <header>
+        <img src={Logo} alt="GoRestaurant" />
+        <nav>
+          <div>
+            <button
+              type="button"
+              onClick={openModal}
+            >
+              <div className="text">Novo Prato</div>
+              <div className="icon">
+                <FiPlusSquare size={24} />
+              </div>
+            </button>
+          </div>
+        </nav>
+      </header>
+    </Container>
+  );
+}
+
+/*
 class Header extends Component {
   render() {
     const { openModal } = this.props;
@@ -30,5 +58,7 @@ class Header extends Component {
     )
   }
 };
+*/
+
 
 export default Header;
